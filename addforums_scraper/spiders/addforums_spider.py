51263178aab9dd028b7833164a2b0bb0a9b656ef
napman <napman02@gmail.com>
2016-04-05 20:19:05 +0500

    subforums fix

--- addforums_scraper/spiders/addforums_spider.py
+++ addforums_scraper/spiders/addforums_spider.py
@@ -30,10 +30,11 @@ class AddforumsSpider(scrapy.Spider):
             yield scrapy.Request(response.urljoin(url), callback=self.parse_posts, priority=1)
 
         # check subforums
-        forum_urls = response.xpath('.//td[contains(@id,"f")]/div/a/@href').extract()
-        for url in forum_urls:
-            url = w3lib.url.url_query_cleaner(url, ['s'], remove=True)
-            yield scrapy.Request(response.urljoin(url), callback=self.parse_forum)
+        if 'paginate' not in response.meta:
+            forum_urls = response.xpath('.//td[contains(@id,"f")]//td/div/a/@href').extract()
+            for url in forum_urls:
+                url = w3lib.url.url_query_cleaner(url, ['s'], remove=True)
+                yield scrapy.Request(response.urljoin(url), callback=self.parse_forum)
 
         # return the next forum page if it exists
         yield self.paginate(response, next_page_callback=self.parse_forum)
@@ -46,6 +47,20 @@ class AddforumsSpider(scrapy.Spider):
         else:
             posts_scraped = []
 
+        title = ''
+        title_list = response.xpath('.//td[@class="navbar"]/strong/text()').extract()
+        if title_list is not None:
+            title_list = [unicode.strip(t) for t in title_list if unicode.strip(t)]
+            title = ' '.join(title_list)
+
+        today = datetime.datetime.utcnow() - datetime.timedelta(hours=4)
+        yesterday = today - datetime.timedelta(days=1)
+
+        if response.xpath('.//div[@class="pagenav"]//a').extract_first() is not None:
+            pagination = True
+        else:
+            pagination = False
+
         for post in response.xpath("//table[contains(@id,'post')]"):
             p = PostItem()
 
@@ -56,15 +71,11 @@ class AddforumsSpider(scrapy.Spider):
             p['uniqueID'] = str(p['qid']) + '_'
             p['uniqueID'] += 'top' if p['localID'] == -1 else str(p['localID'])
 
-            title_list = response.xpath('.//td[@class="navbar"]/strong/text()').extract()
-            title_list = [unicode.strip(t) for t in title_list if unicode.strip(t)]
-            p['title'] = ' '.join(title_list)
+            p['title'] = title
 
             p['poster'] = post.xpath(".//a[@class='bigusername']//text()").extract_first()
 
             p_datetime = post.xpath("string(.//tr/td/div[@class='normal'][2])").extract_first().strip()
-            today = datetime.datetime.utcnow() - datetime.timedelta(hours=4)
-            yesterday = today - datetime.timedelta(days=1)
             p_datetime = re.sub("^Yesterday", yesterday.strftime('%m-%d-%y'), p_datetime)
             p_datetime = re.sub("^Today", today.strftime('%m-%d-%y'), p_datetime)
             p_datetime = datetime.datetime.strptime(p_datetime, "%m-%d-%y, %I:%M %p")
@@ -96,7 +107,8 @@ class AddforumsSpider(scrapy.Spider):
 
             yield p
 
-        yield self.paginate_posts(response, next_page_callback=self.parse_posts, posts_scraped=posts_scraped)
+        if pagination:
+            yield self.paginate_posts(response, next_page_callback=self.parse_posts, posts_scraped=posts_scraped)
 
     def paginate(self, response, next_page_callback):
         """Returns a scrapy.Request for the next page, or returns None if no next page found.
@@ -107,6 +119,7 @@ class AddforumsSpider(scrapy.Spider):
             url = response.urljoin(next_page.extract_first())
             logging.info("NEXT PAGE IS: %s" % url)
             request = scrapy.Request(url, next_page_callback)
+            request.meta['paginate'] = True
             return request
         else:
             return None
